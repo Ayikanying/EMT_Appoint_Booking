d0dfeb6fac3c2b66275beb4cc80f329dd8c73118
Ayikanying <allanayikanying72@gmail.com>
2026-01-22 09:09:07 +0300

    added payment model

--- core/models.py
+++ core/models.py
@@ -22,7 +22,6 @@ class Appointment(models.Model):
 
     STATUS_CHOICES = [
         ('PENDING', 'Pending'),
-        ('PAID', 'Paid'),
         ('APPROVED', 'Approved'),
         ('REJECTED', 'Rejected'),
         ('COMPLETED', 'Completed'),
@@ -63,4 +62,6 @@ class Payment(models.Model):
     appointment = models.OneToOneField(Appointment, on_delete=models.CASCADE, related_name='payments')
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='PENDING')
+    payment_method = models.CharField(max_length=20, choices=PAYMENT_METHODS)
+    transaction_id = models.CharField(max_length=100, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
